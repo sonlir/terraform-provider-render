9fac49b7921eda5de9c7e6ceae748d99cca0cd69
Viktor Semenov <viktor_semenov2@epam.com>
2024-03-12 23:46:29 +0300

    add docs

---
+++ examples/data-sources/render_web_services/data-source.tf
@@ -0,0 +1,5 @@
+data "render_web_services" "example" {}
+
+data "render_web_services" "example" {
+  name = "render-web-service"
+}
---
+++ examples/resources/render_web_service/import.sh
@@ -0,0 +1,2 @@
+# WebService can be imported by specifying the id.
+terraform import render_web_service.example srv-cabcdefghijklmnopqest
